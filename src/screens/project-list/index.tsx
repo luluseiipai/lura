bf58c0c3e1429aea0da3ce1755fff1e65c406fa0
NikQ <780366011@qq.com>
2021-07-21 18:46:39 +0800

    feat: 工程列表

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react'
+import * as qs from 'qs'
+import { List } from './list'
+import { SearchPanel } from './search-panel'
+import { cleanObject, useDebounce } from 'utils'
+
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+  const [list, setList] = useState(undefined)
+  const [users, setUsers] = useState([])
+  const [param, setParam] = useState({
+    name: '',
+    personId: '',
+  })
+  const debounceParam = useDebounce(param, 1000)
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async (res) => {
+      if (res.ok) {
+        setList(await res.json())
+      }
+    })
+  }, [debounceParam])
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (res) => {
+      if (res.ok) {
+        setUsers(await res.json())
+      }
+    })
+  }, [])
+
+  return (
+    <div>
+      <SearchPanel param={param} users={users} setParam={setParam} />
+      <List list={list} users={users} />
+    </div>
+  )
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value)
+
+// 在一个函数里，改变传入的对象本身是不好的
+export const cleanObject = (object?: { [key: string]: unknown }) => {
+  if (!object) {
+    return {}
+  }
+  const result = { ...object }
+  Object.keys(result).forEach((key) => {
+    const value = result[key]
+    if (isFalsy(value)) {
+      delete result[key]
+    }
+  })
+  return result
+}
+
+export const useDebounce = <T>(value: T, delay = 300) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+
+  return debouncedValue
+}
