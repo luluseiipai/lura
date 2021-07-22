143e1472f514baf4cbb6a3469fce2f8178a24ab0
NikQ <780366011@qq.com>
2021-07-22 18:18:46 +0800

    feat: login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ProjectListScreen } from 'screens/project-list'
 import 'App.css'
+import { LoginScreen } from 'screens/login'
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      <LoginScreen />
+      {/* <ProjectListScreen /> */}
     </div>
   )
 }
