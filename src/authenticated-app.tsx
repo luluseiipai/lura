98bb4131eecf76d760651b09279c9ae530c5dfd4
NikQ <780366011@qq.com>
2021-07-28 01:34:54 +0800

    style: 用flex布局

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -9,7 +9,14 @@ export const AuthenticatedApp = () => {
   return (
     <Container>
       <Header>
-        <button onClick={logout}>登出</button>
+        <HeaderLeft>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
       </Header>
       <Nav>nav</Nav>
       <Main>
@@ -37,7 +44,14 @@ const Container = styled.div`
 const Header = styled.header`
   grid-area: header;
   display: flex;
+  align-items: center;
+  justify-content: space-between;
 `
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`
+const HeaderRight = styled.div``
 const Main = styled.main`
   grid-area: main;
 `
