8dfd25e04a7c7074d583792ce9ac10fb41f6bd45
NikQ <780366011@qq.com>
2021-07-28 01:39:20 +0800

    style: 调整布局

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -18,12 +18,9 @@ export const AuthenticatedApp = () => {
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>aside</Aside>
-      <Footer>footer</Footer>
     </Container>
   )
 }
@@ -32,17 +29,9 @@ const Container = styled.div`
   height: 100vh;
   display: grid;
   grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    'header header header'
-    'nav main aside'
-    'footer footer footer';
-  grid-gap: 2px;
 `
 
-// grid-area: 用来给 grid 子元素起名字
 const Header = styled.header`
-  grid-area: header;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -52,15 +41,4 @@ const HeaderLeft = styled.div`
   align-items: center;
 `
 const HeaderRight = styled.div``
-const Main = styled.main`
-  grid-area: main;
-`
-const Nav = styled.nav`
-  grid-area: nav;
-`
-const Aside = styled.aside`
-  grid-area: aside;
-`
-const Footer = styled.footer`
-  grid-area: footer;
-`
+const Main = styled.main``
