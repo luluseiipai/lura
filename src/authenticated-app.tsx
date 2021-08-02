ef32b02585d0eb05d21f971f8f8ba92ad5eca824
NikQ <780366011@qq.com>
2021-08-02 18:17:20 +0800

    feat: 加入路由功能，项目列表路由、项目详情路由

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,42 +1,57 @@
 import { useAuth } from 'context/auth-context'
-import { ProjectListScreen } from 'screens/project-list'
 import { Row } from 'components/lib'
+import { ProjectListScreen } from 'screens/project-list'
+import { ProjectScreen } from 'screens/project'
+import { Navigate, Route, Routes } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 
 import styled from '@emotion/styled'
 import { ReactComponent as SoftwareLogo } from 'assets/software-logo.svg'
 import { Button, Dropdown, Menu } from 'antd'
 
-export const AuthenticatedApp = () => {
+const PageHeader = () => {
   const { logout, user } = useAuth()
 
   return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <SoftwareLogo width="18rem" color="rgb(38, 132, 255)" />
+        <h3>项目</h3>
+        <h3>用户</h3>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key="logout">
+                <Button type="link" onClick={logout}>
+                  登出
+                </Button>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button type="link" onClick={(e) => e.preventDefault()}>
+            Hi, {user?.name}
+          </Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
+  )
+}
+
+export const AuthenticatedApp = () => {
+  return (
     <Container>
-      <Header between={true}>
-        <HeaderLeft gap={true}>
-          <SoftwareLogo width="18rem" color="rgb(38, 132, 255)" />
-          <h3>项目</h3>
-          <h3>用户</h3>
-        </HeaderLeft>
-        <HeaderRight>
-          <Dropdown
-            overlay={
-              <Menu>
-                <Menu.Item key="logout">
-                  <Button type="link" onClick={logout}>
-                    登出
-                  </Button>
-                </Menu.Item>
-              </Menu>
-            }
-          >
-            <Button type="link" onClick={(e) => e.preventDefault()}>
-              Hi, {user?.name}
-            </Button>
-          </Dropdown>
-        </HeaderRight>
-      </Header>
+      <PageHeader />
       <Main>
         <ProjectListScreen />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/projects" element={<ProjectListScreen />} />
+            <Route path="/projects/:projectId/*" element={<ProjectScreen />} />
+          </Routes>
+        </BrowserRouter>
       </Main>
     </Container>
   )
