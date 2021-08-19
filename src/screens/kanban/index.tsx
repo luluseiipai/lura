542dab56a3300776c4a467add8c1d4587a193e30
NikQ <780366011@qq.com>
2021-08-19 17:55:22 +0800

    feat: 添加 tasks Search 功能

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled'
 import { useDocumentTitle } from 'utils'
 import { useKanban } from 'utils/kanban'
 import { KanbanColumn } from './kanban-column'
+import { SearchPanel } from './search-panel'
 import { useKanbanSearchParams, useProjectInUrl } from './util'
 
 export const KanBanScreen = () => {
@@ -13,6 +14,7 @@ export const KanBanScreen = () => {
   return (
     <div>
       <h1>{currentProject?.name}看板</h1>
+      <SearchPanel />
       <ColumnContainer>
         {kanbans.map((kanban) => (
           <KanbanColumn kanban={kanban} key={kanban.id} />
--- src/screens/kanban/util.ts
+++ src/screens/kanban/util.ts
@@ -1,5 +1,7 @@
+import { useMemo } from 'react'
 import { useLocation } from 'react-router'
 import { useProject } from 'utils/project'
+import { useUrlQueryParam } from 'utils/url'
 
 export const useProjectIdInUrl = () => {
   const { pathname } = useLocation()
@@ -17,7 +19,20 @@ export const useKanbanSearchParams = () => ({ projectId: useProjectIdInUrl() })
 export const useKanbanQueryKey = () => ['kanbans', useKanbanSearchParams()]
 
 /**任务搜索参数 */
-export const useTasksSearchParams = () => ({ projectId: useProjectIdInUrl() })
+export const useTasksSearchParams = () => {
+  const [param] = useUrlQueryParam(['name', 'typeId', 'processorId', 'tagId'])
+  const projectId = useProjectIdInUrl()
+  return useMemo(
+    () => ({
+      projectId,
+      typeId: Number(param.typeId) || undefined,
+      processorId: Number(param.processorId) || undefined,
+      tagId: Number(param.tagId) || undefined,
+      name: param.name,
+    }),
+    [projectId, param]
+  )
+}
 
 /**任务queryKey */
 export const useTasksQueryKey = () => ['tasks', useTasksSearchParams()]
